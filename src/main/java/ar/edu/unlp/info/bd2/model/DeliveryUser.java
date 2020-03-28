5210e39459c75d4c00f4ded8c6b0ef49b7331199
ramirezlp <agusedlp1@gmail.com>
2020-03-28 18:09:33 -0300

    agrego anotations en algunas clases y modifico constructor de duser

--- src/main/java/ar/edu/unlp/info/bd2/model/DeliveryUser.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/DeliveryUser.java
@@ -1,9 +1,13 @@
 package ar.edu.unlp.info.bd2.model;
 
+import java.util.Date;
+
 public class DeliveryUser extends User {
 
-	public DeliveryUser() {
-		super();
+	public DeliveryUser(String email, String pass, String username, String name, Date date) {
+		super(email, pass, username, name, date);
+		// TODO Auto-generated constructor stub
 	}
+
 
 }
