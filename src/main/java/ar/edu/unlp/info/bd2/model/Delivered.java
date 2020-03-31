d79709907ae709a1093da49fa4bd711d1caafc95
tomasuno99 <tomassandler99@gmail.com>
2020-03-31 17:39:20 -0300

    modelo status en bd

--- src/main/java/ar/edu/unlp/info/bd2/model/Delivered.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Delivered.java
@@ -1,6 +1,10 @@
 package ar.edu.unlp.info.bd2.model;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
-public class Delivered implements OrderStatus {
+@Entity
+@DiscriminatorValue("3")
+public class Delivered extends OrderStatus {
 
 	@Override
 	public String getStatus() {
--- src/main/java/ar/edu/unlp/info/bd2/model/Sended.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Sended.java
@@ -1,6 +1,10 @@
 package ar.edu.unlp.info.bd2.model;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
-public class Sended implements OrderStatus {
+@Entity
+@DiscriminatorValue("2")
+public class Sended extends OrderStatus {
 
 	public String getStatus() {
 		return "Sended";
