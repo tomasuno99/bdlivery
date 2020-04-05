ae8310cc67c13b9358ad5c08c957b61c60714c47
tomasuno99 <tomassandler99@gmail.com>
2020-04-05 19:49:37 -0300

    cambio createDatabase

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,4 +1,4 @@
-mysql -u root -e "drop database if exists bd2_grupo15;
+mysql -p -u root -e "drop database if exists bd2_grupo15;
                   create database bd2_grupo15;
                   CREATE USER IF NOT EXISTS grupo15 IDENTIFIED BY 'bd2';
                   GRANT ALL PRIVILEGES ON bd2_grupo15.* TO 'grupo15'@'localhost';
