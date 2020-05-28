47f293448fb7219d179e0331489783d8a8d27ffc
tomasuno99 <tomassandler99@gmail.com>
2020-05-28 13:23:07 -0300

    estructura para tp3

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DBlivery
-Grupo 15<br>
-Sandler	Tomas	15503/0	<br>
-Ramirez	Agustin	15374/9<br>
+Proyecto Base para el TP de la cátedra de Bases de Datos 2
 Facultad de Informática / UNLP
+
+Sandler, Tomas
+Ramirez, Agustin
--- createDatabase.sh
+++
@@ -1,6 +0,0 @@
-mysql -p -u root -e "drop database if exists bd2_grupo15;
-                  create database bd2_grupo15;
-                  CREATE USER IF NOT EXISTS grupo15 IDENTIFIED BY 'bd2';
-                  GRANT ALL PRIVILEGES ON bd2_grupo15.* TO 'grupo15'@'localhost';
-                  FLUSH PRIVILEGES;
-                  "
